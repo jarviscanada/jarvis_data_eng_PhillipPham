2f8ef218cb60d0cfc12f67929a8294da5e2ea15b
Phillip Pham <phoneqw@gmail.com>
2026-04-06 15:11:27 +0000

    cleaned up bash script comments

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -5,15 +5,14 @@ cmd=$1
 db_username=$2
 db_password=$3
 
-# Start docker
-# Make sure you understand the double pipe operator
+# Start docker if not already running
 sudo systemctl status docker || systemctl start docker
 
-# Check container status (try the following cmds on terminal)
+# Check container status
 docker container inspect jrvs-psql
 container_status=$?
 
-# User switch case to handle create|stop|start opetions
+# User switch case to handle create|stop|start options
 case $cmd in
   create)
 
@@ -36,7 +35,7 @@ case $cmd in
 	-e POSTGRES_USER="$db_username" -d -v pgdata:/var/lib/postgresql/data \
 	-p 5432:5432 postgres:9.6-alpine
 
-  # Make sure you understand what's `$?`
+  # return exit status of 'run' command
 	exit $?
 	;;
 
